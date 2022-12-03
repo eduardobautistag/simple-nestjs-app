137025e37eefe9948ab62ba1d5378241d08fd06e
Eduardo Bautista Gonzales <93616861+eduardobautistag@users.noreply.github.com>
2022-12-03 16:18:33 -0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Eduardo from pipeline 3000!';
+    return 'Hello World Eduardo from pipeline Hoy Sabado!';
   }
 }
